db18000c01d3464ec6e19e0a9b71c5891473c247
Ken <Ken@816e059b-bcfe-4948-801d-599fda27caa5>
2009-10-12 06:22:29 +0000

    Winamp SDK ヘッダファイル更新
    git-svn-id: file:///cygdrive/c/WORK/mp3infp/SVNReps/branches/unicode@18 816e059b-bcfe-4948-801d-599fda27caa5

--- src/ipc_pe.h
+++ src/ipc_pe.h
@@ -13,11 +13,20 @@
 #define IPC_PE_GETIDXFROMPOINT    109 // pass a point parm, return a playlist index
 #define IPC_PE_SAVEEND            110 // pass index to save from
 #define IPC_PE_RESTOREEND         111 // no parm
-
+#define IPC_PE_GETNEXTSELECTED    112 // same as IPC_PLAYLIST_GET_NEXT_SELECTED for the main window
+#define IPC_PE_GETSELECTEDCOUNT   113
+#define IPC_PE_INSERTFILENAMEW  114 // (copydata) lpData is of type fileinfoW
+#define IPC_PE_GETINDEXINFO_TITLE 115 // like IPC_PE_GETINDEXINFO, but writes the title to char file[MAX_PATH] instead of filename
+#define IPC_PE_GETINDEXINFORESULT_TITLE 116 // callback message for IPC_PE_GETINDEXINFO
 typedef struct {
 	char file[MAX_PATH];
 	int index;
 	} fileinfo;
+
+typedef struct {
+	wchar_t file[MAX_PATH];
+	int index;
+	} fileinfoW;
 
 typedef struct {
 	HWND callback;
@@ -27,11 +36,21 @@ typedef struct {
 // the following messages are in_process ONLY
 
 #define IPC_PE_GETINDEXTITLE      200 // lParam = pointer to fileinfo2 struct
+#define IPC_PE_GETINDEXTITLEW      201 // lParam = pointer to fileinfo2W struct
+#define IPC_PE_GETINDEXINFO_INPROC      202 // lParam = pointer to fileinfo struct
+#define IPC_PE_GETINDEXINFOW_INPROC      203 // lParam = pointer to fileinfoW struct
 
 typedef struct {
   int fileindex;
   char filetitle[256];
   char filelength[16];
   } fileinfo2;
+
+typedef struct
+{
+  int fileindex;
+  wchar_t filetitle[256];
+  wchar_t filelength[16];
+  } fileinfo2W;
 
 #endif
