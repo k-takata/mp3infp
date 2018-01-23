66ec757c02cf99623d902ef174586b43b86f664d
K.Takata <kentkt@csc.jp>
2018-01-23 23:49:21 +0900

    Installer: Update a script
    mp3infp_regist.exe has been removed.

--- src/Installer/make_installer.bat
+++ src/Installer/make_installer.bat
@@ -34,9 +34,6 @@ copy ..\lang_japanese\objfre_%target_x64%_amd64\amd64\Japanese.lng ..\x64 /y
 copy ..\mp3infp_setup\objfre_%target_x86%_x86\i386\mp3infp_setup.exe    ..\x86 /y
 copy ..\mp3infp_setup\objfre_%target_x64%_amd64\amd64\mp3infp_setup.exe ..\x64 /y
 
-copy ..\mp3infp_regist\objfre_%target_x86%_x86\i386\mp3infp_regist.exe    ..\x86 /y
-copy ..\mp3infp_regist\objfre_%target_x64%_amd64\amd64\mp3infp_regist.exe ..\x64 /y
-
 
 rem インストーラ作成
 "%ProgramFiles%\NSIS\makensis.exe" "/DVERSION=%version%" "/XOutFile mp3infpu%filever%.exe" mp3infp.nsi
