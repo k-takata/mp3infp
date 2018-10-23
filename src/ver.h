e6f4d17ac1b4f800f2739dd496f30556a64e4289
K.Takata <kentkt@csc.jp>
2018-10-23 12:53:00 +0900

    doc: Update document and version number
    Update for Ver.2.55.

--- src/ver.h
+++ src/ver.h
@@ -4,7 +4,7 @@
 //
 #define MAJOR_REL	2
 #define MINOR_REL	55
-#define BUILD_NUM	96
-#define BUILD_STR	"2.55.96.0\0"
-#define PRODUCT_STR	"2.55.96.0\0"
+#define BUILD_NUM	97
+#define BUILD_STR	"2.55.97.0\0"
+#define PRODUCT_STR	"2.55.97.0\0"
 
